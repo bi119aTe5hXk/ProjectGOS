01ae85726e14834168e0bb97393f1d0755141932
bi119aTe5hXk <billgateshxk@gmail.com>
2020-07-22 20:44:57 +0900

    Initial Commit

---
+++ glassos/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  glassos
+//
+//  Created by billgateshxk on 2020/07/22.
+//  Copyright © 2020 bi119aTe5hXk. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ glassos/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  glassos
+//
+//  Created by billgateshxk on 2020/07/22.
+//  Copyright © 2020 bi119aTe5hXk. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

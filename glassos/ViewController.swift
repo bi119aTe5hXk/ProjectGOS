7dae407db6dcb7939e4360cdd0501ba1a3aa66e6
bi119aTe5hXk <billgateshxk@gmail.com>
2020-10-24 15:34:12 +0900

    Format code

--- glassos/ViewController.swift
+++ glassos/ViewController.swift
@@ -13,15 +13,22 @@ import VideoToolbox
 import Vision
 
 class ViewController: NSViewController, AVCaptureVideoDataOutputSampleBufferDelegate{
+    /* Settings */
+    //Model file name
+    let modelName = "JPTrafficSignObjectDetector"
+    //View point moving distance
     let VPMovingDistance: CGFloat = 1.0
-    let userdefault = UserDefaults.standard
     
+    
+    let userdefault = UserDefaults.standard
     @IBOutlet var leftView:NSView!
     @IBOutlet var rightView:NSView!
     @IBOutlet var leftTextField: NSTextField!
     @IBOutlet var rightTextField: NSTextField!
     @IBOutlet var leftImageView: NSImageView!
     @IBOutlet var rightImageView: NSImageView!
+    
+    
     
     var bufferSize: CGSize = .zero
     var rootLayer: CALayer! = nil
@@ -43,13 +50,32 @@ class ViewController: NSViewController, AVCaptureVideoDataOutputSampleBufferDele
         setupAVCapture()
     }
     
-    private let videoDataOutputQueue = DispatchQueue(label: "VideoDataOutput", qos: .userInitiated, attributes: [], autoreleaseFrequency: .workItem)
-    
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-        // to be implemented in the subclass
+        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
+            return
+        }
+        
+        
+        
+        let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, orientation: .up, options: [:])
+        do {
+            try imageRequestHandler.perform(self.requests)
+        } catch {
+            print(error)
+        }
     }
     
     
+    // MARK: - Video Capture
+    private let videoDataOutputQueue = DispatchQueue(label: "VideoDataOutput", qos: .userInitiated, attributes: [], autoreleaseFrequency: .workItem)
+    
+    func startCaptureSession() {
+        session.startRunning()
+    }
+    
+    func captureOutput(_ captureOutput: AVCaptureOutput, didDrop didDropSampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+         print("frame dropped")
+    }
     
     func setupAVCapture() {
         var deviceInput: AVCaptureDeviceInput!
@@ -105,28 +131,61 @@ class ViewController: NSViewController, AVCaptureVideoDataOutputSampleBufferDele
             print(error)
         }
         session.commitConfiguration()
-        //previewLayer = AVCaptureVideoPreviewLayer(session: session)
-        //previewLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
-        //previewLayer.frame = rootLayer.bounds
-//        rootLayer.addSublayer(previewLayer)
-    }
-    
-    func startCaptureSession() {
-        session.startRunning()
-    }
-    
-    // Clean up capture setup
-    func teardownAVCapture() {
-//        previewLayer.removeFromSuperlayer()
-//        previewLayer = nil
-    }
-    
-    func captureOutput(_ captureOutput: AVCaptureOutput, didDrop didDropSampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-        // print("frame dropped")
+        setupVision()
+        
+        // start the capture
+        startCaptureSession()
     }
     
     
+    // MARK: - AI Object Recognition & Display
     
+    
+    // Vision parts
+    private var requests = [VNRequest]()
+    
+    @discardableResult
+    func setupVision() -> NSError? {
+        // Setup Vision parts
+        let error: NSError! = nil
+        
+        guard let modelURL = Bundle.main.url(forResource: modelName, withExtension: "mlmodelc") else {
+            return NSError(domain: "VisionObjectRecognitionViewController", code: -1, userInfo: [NSLocalizedDescriptionKey: "Model file is missing"])
+        }
+        do {
+            let visionModel = try VNCoreMLModel(for: MLModel(contentsOf: modelURL))
+            let objectRecognition = VNCoreMLRequest(model: visionModel, completionHandler: { (request, error) in
+                
+                DispatchQueue.main.async(execute: {
+                    // perform all the UI updates on the main queue
+                    if let results = request.results {
+//                        self.drawVisionRequestResults(results)
+                        
+//                        print("results:\(results)")
+                        self.showVisionRequestResults(results)
+                    }
+                })
+            })
+            self.requests = [objectRecognition]
+        } catch let error as NSError {
+            print("Model loading went wrong: \(error)")
+        }
+        
+        return error
+    }
+    
+    func showVisionRequestResults(_ results: [Any]) {
+        for observation in results where observation is VNRecognizedObjectObservation{
+            guard let objectObservation = observation as? VNRecognizedObjectObservation else {
+                continue
+            }
+            // Select only the label with the highest confidence.
+//            print("objectObservation:\(objectObservation.labels)")
+            
+            let topLabelObservation = objectObservation.labels[0]
+            print("Identifier:\(topLabelObservation.identifier), confidence:\(topLabelObservation.confidence)")
+        }
+    }
     
     // MARK: - Side by side UI
     func showTextOnGlass(string:String){
@@ -211,165 +270,8 @@ class ViewController: NSViewController, AVCaptureVideoDataOutputSampleBufferDele
     }
     
     
-    // MARK: - AI Object Recognition
     
     
-    private var detectionOverlay: CALayer! = nil
     
-    // Vision parts
-    private var requests = [VNRequest]()
-    
-    @discardableResult
-    func setupVision() -> NSError? {
-        // Setup Vision parts
-        let error: NSError! = nil
-        
-        guard let modelURL = Bundle.main.url(forResource: "JPTrafficSignObjectDetector", withExtension: "mlmodelc") else {
-            return NSError(domain: "VisionObjectRecognitionViewController", code: -1, userInfo: [NSLocalizedDescriptionKey: "Model file is missing"])
-        }
-        do {
-            let visionModel = try VNCoreMLModel(for: MLModel(contentsOf: modelURL))
-            let objectRecognition = VNCoreMLRequest(model: visionModel, completionHandler: { (request, error) in
-                print("Model loaded.")
-                DispatchQueue.main.async(execute: {
-                    // perform all the UI updates on the main queue
-                    if let results = request.results {
-//                        self.drawVisionRequestResults(results)
-                        
-                        //print("results:\(results.)")
-                        self.showVisionRequestResults(results)
-                    }
-                })
-            })
-            self.requests = [objectRecognition]
-        } catch let error as NSError {
-            print("Model loading went wrong: \(error)")
-        }
-        
-        return error
-    }
-    
-    func showVisionRequestResults(_ results: [Any]) {
-        for observation in results where observation is VNRecognizedObjectObservation{
-            guard let objectObservation = observation as? VNRecognizedObjectObservation else {
-                continue
-            }
-            // Select only the label with the highest confidence.
-            let topLabelObservation = objectObservation.labels[0]
-            print("Identifier:\(topLabelObservation.identifier), confidence:\(topLabelObservation.confidence)")
-        }
-    }
-    
-    func drawVisionRequestResults(_ results: [Any]) {
-        CATransaction.begin()
-        CATransaction.setValue(kCFBooleanTrue, forKey: kCATransactionDisableActions)
-        detectionOverlay.sublayers = nil // remove all the old recognized objects
-        for observation in results where observation is VNRecognizedObjectObservation {
-            guard let objectObservation = observation as? VNRecognizedObjectObservation else {
-                continue
-            }
-            // Select only the label with the highest confidence.
-            let topLabelObservation = objectObservation.labels[0]
-            let objectBounds = VNImageRectForNormalizedRect(objectObservation.boundingBox, Int(bufferSize.width), Int(bufferSize.height))
-            
-            let shapeLayer = self.createRoundedRectLayerWithBounds(objectBounds)
-            
-            let textLayer = self.createTextSubLayerInBounds(objectBounds,
-                                                            identifier: topLabelObservation.identifier,
-                                                            confidence: topLabelObservation.confidence)
-            shapeLayer.addSublayer(textLayer)
-            detectionOverlay.addSublayer(shapeLayer)
-        }
-        self.updateLayerGeometry()
-        CATransaction.commit()
-    }
-    
-    override func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
-            return
-        }
-        
-        
-        
-        let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, orientation: .up, options: [:])
-        do {
-            try imageRequestHandler.perform(self.requests)
-        } catch {
-            print(error)
-        }
-    }
-    
-    override func setupAVCapture() {
-        super.setupAVCapture()
-        
-        // setup Vision parts
-        setupLayers()
-        updateLayerGeometry()
-        setupVision()
-        
-        // start the capture
-        startCaptureSession()
-    }
-    
-    func setupLayers() {
-        detectionOverlay = CALayer() // container layer that has all the renderings of the observations
-        detectionOverlay.name = "DetectionOverlay"
-        detectionOverlay.bounds = CGRect(x: 0.0,
-                                         y: 0.0,
-                                         width: bufferSize.width,
-                                         height: bufferSize.height)
-        detectionOverlay.position = CGPoint(x: rootLayer.bounds.midX, y: rootLayer.bounds.midY)
-        rootLayer.addSublayer(detectionOverlay)
-    }
-    
-    func updateLayerGeometry() {
-        let bounds = rootLayer.bounds
-        var scale: CGFloat
-        
-        let xScale: CGFloat = bounds.size.width / bufferSize.height
-        let yScale: CGFloat = bounds.size.height / bufferSize.width
-        
-        scale = fmax(xScale, yScale)
-        if scale.isInfinite {
-            scale = 1.0
-        }
-        CATransaction.begin()
-        CATransaction.setValue(kCFBooleanTrue, forKey: kCATransactionDisableActions)
-        
-        // rotate the layer into screen orientation and scale and mirror
-        detectionOverlay.setAffineTransform(CGAffineTransform(rotationAngle: CGFloat(.pi / 2.0)).scaledBy(x: scale, y: -scale))
-        // center the layer
-        detectionOverlay.position = CGPoint(x: bounds.midX, y: bounds.midY)
-        
-        CATransaction.commit()
-        
-    }
-    
-    func createTextSubLayerInBounds(_ bounds: CGRect, identifier: String, confidence: VNConfidence) -> CATextLayer {
-        let textLayer = CATextLayer()
-        textLayer.name = "Object Label"
-        let formattedString = NSMutableAttributedString(string: String(format: "\(identifier)\nConfidence:  %.2f", confidence))
-        let largeFont = NSFont(name: "Helvetica", size: 24.0)!
-        formattedString.addAttributes([NSAttributedString.Key.font: largeFont], range: NSRange(location: 0, length: identifier.count))
-        textLayer.string = formattedString
-        textLayer.bounds = CGRect(x: 0, y: 0, width: bounds.size.height - 10, height: bounds.size.width - 10)
-        textLayer.position = CGPoint(x: bounds.midX, y: bounds.midY)
-        textLayer.shadowOpacity = 0.7
-        textLayer.shadowOffset = CGSize(width: 2, height: 2)
-        textLayer.foregroundColor = CGColor(colorSpace: CGColorSpaceCreateDeviceRGB(), components: [0.0, 0.0, 0.0, 1.0])
-        textLayer.contentsScale = 2.0 // retina rendering
-        // rotate the layer into screen orientation and scale and mirror
-        textLayer.setAffineTransform(CGAffineTransform(rotationAngle: CGFloat(.pi / 2.0)).scaledBy(x: 1.0, y: -1.0))
-        return textLayer
-    }
-    
-    func createRoundedRectLayerWithBounds(_ bounds: CGRect) -> CALayer {
-        let shapeLayer = CALayer()
-        shapeLayer.bounds = bounds
-        shapeLayer.position = CGPoint(x: bounds.midX, y: bounds.midY)
-        shapeLayer.name = "Found Object"
-        shapeLayer.backgroundColor = CGColor(colorSpace: CGColorSpaceCreateDeviceRGB(), components: [1.0, 1.0, 0.2, 0.4])
-        shapeLayer.cornerRadius = 7
-        return shapeLayer
-    }
+
 }
